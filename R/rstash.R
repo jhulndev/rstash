1a5433c3f3f3742404b4d2372139500f7acdfc76
Jason Huling <jason.huling@gmail.com>
2016-07-08 10:23:49 -0700

    Updated rstash help and class documentation and added xml2 to imports

--- R/rstash.R
+++ R/rstash.R
@@ -1,39 +1,49 @@
-#' rstash: A package for...
+#' rstash: A package for abstracting away where your data is being stored,
+#'    giving a consistent means of accessing and moving your data whether it is
+#'    on your local file system or in a cloud based object storage system.
 #'
-#' The foo package provides three categories of important functions:
-#' foo, bar and baz.
+#' The rstash package provides two main categories of functions:
+#' Data movement and Informational
 #'
-#' @section Foo functions:
-#' The foo functions ...
+#' @section Data movement functions:
+#' save_stash
+#' get_stash
+#' delete_stash
+#' copy_stash
+#' move_stash
 #'
+#' @section Informational functions:
+#' stash_match
+#' stash_content
 #'
 #' @import plyr
 #' @import aws.s3
 #' @import uuid
+#' @import xml2
 #'
 #' @docType package
 #' @name rstash
 NULL
 
 
-
-
-#' Title
+#' Object for representing an AWS S3 file or directory
 #'
-#' @param bucket s
-#' @param path s
+#' @param bucket AWS S3 bucket
+#' @param path A "file path" to a AWS S3 directory or file. S3 doesn't
+#'    technically have folders/directories, but the object keys can be
+#'    interpreted that way.
 #'
-#' @return a
+#' @return A s3_stash object.
 #' @export
 s3_stash <- function(bucket, path = '') {
   structure(path, class = 's3_stash', bucket = bucket)
 }
 
-#' Title
+#' Object for representing a local file or directory
 #'
-#' @param path s
+#' @param path A file path to a local directory or file.
 #'
-#' @return s
+#' @return A local_stash object.
 #' @export
 local_stash <- function(path = '') {
   structure(path, class = 'local_stash')
